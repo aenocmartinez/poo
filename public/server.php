594a68a652b175125f93679bf0fd09abce49ed7f
abimelecmartinez <enoc.martinez@pulzo.com>
2025-01-15 16:38:07 -0500

    prueba2

--- public/server.php
+++ public/server.php
@@ -1,15 +1,4 @@
 <?php
 
-$valorSalud = $_POST['input_salario_minimo'] * (12/100);
+echo $_POST['input_salario_minimo'] * (12/100);
 
-?>
-
-<script>
-function redirectTo(usuario) {
-    const baseUrl = 'http://poo.test/';
-    const urlWithParams = `${baseUrl}?valorSalud=${encodeURIComponent(usuario)}`;
-    window.location.href = urlWithParams;
-}
-
-redirectTo('<?php echo $valorSalud; ?>');
-</script>
