2eb9f0aaa06ddfff1f5b26a32b9ff8e11afdc2be
abimelecmartinez <enoc.martinez@pulzo.com>
2025-01-15 16:31:38 -0500

    Ejercicio AJAX 1

--- public/index.php
+++ public/index.php
@@ -2,22 +2,27 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// URL del feed RSS
-$rssFeedUrl = 'https://loquepasaencolombia.com/feed/pulzo';
-
-// Cargar el contenido del feed
-$rssFeed = simplexml_load_file($rssFeedUrl);
-
-// Verificar si se ha cargado correctamente
-if ($rssFeed === false) {
-    echo "Error al cargar el feed RSS.";
-    exit;
+if ( isset($_GET['valorSalud'])) {
+    echo "<p>VALOR SALUD: " . $_GET['valorSalud'] . "</p>";
 }
 
-// Recorrer y mostrar los elementos del feed
-foreach ($rssFeed->channel->item as $item) {
-    echo "<h2>{$item->title}</h2>";
-    echo "<p><strong>Fecha de publicación:</strong> {$item->pubDate}</p>";
-    echo "<p><strong>Descripción:</strong> {$item->description}</p>";
-    echo "<p><a href='{$item->link}'>Leer más</a></p><hr>";
-}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Samyr</title>
+</head>
+<body>
+    
+    <form action="server.php" method="post">
+        <label for="SalarioMinimo">Salario Minimo</label>
+        <input type="number" min="0" name="input_salario_minimo" id="input_salario_minimo" value="1300000">
+        <input type="submit" value="Enviar">
+    </form>
+
+
+</body>
+</html>
